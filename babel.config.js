b78252b1eca683aadc4eecfba0933d4ca811e6ab
Marina <marina.p@carbonclick.com>
2024-05-02 18:12:07 +0200

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,14 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: false,
-      },
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: false,
+        },
+      ],
     ],
   };
 };
